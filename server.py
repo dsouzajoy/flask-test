3d21e89b7573f75a34679e7f8512b020da097990
dsouzajoy <joy.d@servify.in>
2021-03-10 22:40:41 +0530

    first deploy

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
-
+from flask_cors import CORS
 app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
+CORS(app)
 
 
 @app.errorhandler(404)
@@ -17,9 +18,10 @@ def index():
 def get_test_data():
     return {
         "name": 'test',
-        "description": 'This is test data',
+        "description": 'This is test data from server',
     }
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
